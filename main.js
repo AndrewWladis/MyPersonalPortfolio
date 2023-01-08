25724b9e8d01d2c50138458dc2126e2b018f8cec
AndrewWladis <andrewwladis@gmail.com>
2023-01-08 14:18:42 -0500

    Updated raimi soundboard

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 new TypeIt("#demo", {
     speed: 150,
-    strings: ["JavaScript", "React", "node.js", "front end"],
+    strings: ["JavaScript", "React", "node.js", "front end", "software", "web"],
     breakLines: false,
     loop: true,
     startDelay: 1000,
