cb363d95de7e63f29e41319db72a79a72d4eec15
Andrew Wladis <andrewwladis@gmail.com>
2023-04-07 23:17:55 -0400

    Added Quote Cook to portfolio

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 new TypeIt("#demo", {
     speed: 150,
-    strings: ["JavaScript", "React", "node.js", "front end", "software", "web"],
+    strings: ["JavaScript", "React Native", "full stack", "web", "mobile"],
     breakLines: false,
     loop: true,
     startDelay: 1000,
